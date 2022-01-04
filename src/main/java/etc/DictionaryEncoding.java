5ae9320a7718a10d449ca4802a4835e187adc871
Crispy <gunho19945@gmail.com>
2022-01-04 22:20:45 +0900

    [개발] 멀티 스레드

--- src/main/java/etc/DictionaryEncoding.java
+++ src/main/java/etc/DictionaryEncoding.java
@@ -3,50 +3,52 @@ package etc;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DictionaryEncoding {
-    private Integer key;
-    private Map<Integer, String> dictionary;
-    private Map<String, Integer> reversed;
+    private AtomicInteger key;
+    private Map<AtomicInteger, String> dictionary;
+    private Map<String, AtomicInteger> reversed;
 
     public DictionaryEncoding(){
         dictionary = new HashMap<>();
         reversed = new HashMap<>();
     }
 
-    public DictionaryEncoding(Map<Integer, String> dictionary){
+    public DictionaryEncoding(Map<AtomicInteger, String> dictionary){
         this.dictionary = dictionary;
         this.reversed = new HashMap<>();
     }
 
-    Integer encode(String v){
+    AtomicInteger encode(String v){
         if(!reversed.containsKey(v)){
             reversed.put(v, key);
             dictionary.put(key, v);
-            key += 1;
+            key.addAndGet(1);
             return key;
         } else
             return reversed.get(v);
     }
 
-    List<Integer> encode(List<String> list){
-        List<Integer> res = null;
+    List<AtomicInteger> encode(List<String> list){
+        List<AtomicInteger> res = null;
         for(String str: list){
             res.add(encode(str));
         }
         return res;
     }
 
-    String decode(Integer i){
+    String decode(AtomicInteger i){
         if(!dictionary.containsKey(i)){
             return null;
         } else
             return dictionary.get(i);
     }
 
-    List<String> decode(List<Integer> list){
+    List<String> decode(List<AtomicInteger> list){
         List<String> res = null;
-        for(Integer i : list){
+        for(AtomicInteger i : list){
             res.add(decode(i));
         }
         return res;
