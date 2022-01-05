12b2cc7cf36a3efe8d89c40d106756564a5afc84
Crispy <gunho19945@gmail.com>
2022-01-05 23:54:40 +0900

    [개발] 동시성 보장

---
+++ src/main/java/practice/MultiThread.java
@@ -0,0 +1,46 @@
+package practice;
+
+import java.util.List;
+import java.util.Vector;
+
+public class MultiThread {
+    private int a = 0;
+    private int b = 0;
+    private List<Holder> holders = new Vector<>();
+
+    void update(){
+        Holder holder = new Holder();
+        holder.a = a++;
+        try(){
+            Thread t = new Thread();
+            t.sleep(10);
+        } catch(Exception e){
+            //Nothing to do
+        }
+        holder.b = b++;
+    }
+
+    public class Holder{
+        int a;
+        int b;
+    }
+
+    void run(){
+        List<Thread> threads;
+        for(int i = 0; i < 100; i++){
+            try {
+                Thread t = new Thread();
+                t.sleep(10);
+                update();
+            } catch (Exception e){
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new MultiThread().run();
+    }
+
+}
